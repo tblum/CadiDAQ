ba7094c8c73e5c7786cf22cdce4725c01dc17966
Hanno Perrey <hanno.perrey@nuclear.lu.se>
2017-06-22 08:31:40 +0200

    added CAEN digitizer library headers
    - needed to test enum2str generator code
    - to be removed again and replaced by FindCAENDrv.cmake (or similar)

---
+++ caen/enum2Str.hpp
@@ -0,0 +1,16 @@
+/*!
+  * \file enum2Str.hpp
+  * \warning This is an automatically generated file!
+  */
+
+#pragma once
+
+// clang-format off
+
+#include <string>
+#include <CAENDigitizerType.h>
+
+namespace cadidaq {
+
+class enum2Str {
+ public:
