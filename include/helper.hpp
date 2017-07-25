e963a7b1d77da2174e49f9e8e8e4e33c488bf880
Hanno Perrey <hanno.perrey@nuclear.lu.se>
2017-07-25 15:38:54 +0200

    refactorized code into helper header

---
+++ include/helper.hpp
@@ -0,0 +1,38 @@
+// settings.hpp
+#ifndef CADIDAQ_HELPER_H
+#define CADIDAQ_HELPER_H
+
+#include <boost/optional.hpp>
+
+
+/* converts a vector of optional<bool> into a uint32 bit mask.
+   defaults to 0 if optional not set */
+uint32_t vec2Mask(std::vector<boost::optional<bool>> vec, uint groupsize = 1){
+  std::bitset<32> mask(0);
+  for (auto it = vec.begin(); it != vec.end(); ++it) {
+    auto index = std::distance(vec.begin(), it);
+    // set value if set and 'true' (else it stays 0)
+    if (*it && **it) {
+      uint groupidx = index%groupsize;
+      // treat "special case" of all channels belonging to the same group (e.g. no groups, just channels)
+      if (groupsize == 1)
+        groupidx = index;
+      // create a bit pattern for the group the channel belongs to
+      uint32_t shift = (((uint32_t)1 << groupsize) - 1) << groupidx;
+      // set that pattern
+      mask |= shift;
+    }
+  }
+  return mask.to_ulong();
+}
+
+void mask2Vec(uint32_t mask, std::vector<boost::optional<bool>> vec, uint groupsize = 1){
+std::bitset<32> bits(mask);
+// TODO: implement treatment of groupsizes > 1
+for (auto it = vec.begin(); it != vec.end(); ++it) {
+auto index = std::distance(vec.begin(), it);
+*it = bits[index];
+}
+}
+
+#endif
